5e71279f6574eb8bc9d69ab569e3be151809105e
GoWebtest <2108279047@qq.com>
2022-04-22 12:34:21 +0800

    注释语句

---
+++ .vuepress/config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  "title": "xiaobai's blog",
+  "description": "Hi,there...",
+  "base": "/blog/",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Notes",
+        "icon": "reco-document",
+        "items": [
+          {
+            "text": "Frontend",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/coder-usr",
+            "icon": "reco-github"
+          },
+          {
+            "text": "Gitee",
+            "link": "https://gitee.com/gowebtest",
+            "icon": "reco-mayun"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+        
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "xiaobai",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
